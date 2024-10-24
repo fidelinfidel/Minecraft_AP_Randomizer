a48f5711e2407a94bc42553777bd5b20da25d3b1
Travis <tfryguy09@gmail.com>
2024-10-23 23:51:32 -0400

    Pogress

--- src/main/java/gg/archipelago/aprandomizer/common/events/onBlockBreak.java
+++ src/main/java/gg/archipelago/aprandomizer/common/events/onBlockBreak.java
@@ -2,6 +2,9 @@ package gg.archipelago.aprandomizer.common.events;
 
 import gg.archipelago.aprandomizer.APRandomizer;
 import net.minecraft.network.chat.Component;
+import net.minecraft.world.item.Item;
+import net.minecraft.world.item.ItemStack;
+import net.minecraft.world.level.ItemLike;
 import net.minecraftforge.event.level.BlockEvent;
 import net.minecraftforge.eventbus.api.SubscribeEvent;
 import net.minecraftforge.fml.common.Mod;
@@ -28,7 +31,11 @@ public class onBlockBreak {
         else
         {            
             //Good Place to beeeeee
-            
+
+            event.getPlayer().sendSystemMessage(Component.literal("Good Jorb!"));
+
+            ItemStack stack = new ItemStack(Item.byId(424).asItem());
+            event.getPlayer().addItem(stack);
         }
     }
 }
